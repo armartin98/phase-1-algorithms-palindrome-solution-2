8ee98ee17303e2fc83769e6357d3324482e31121
Andrew <armartin1998@gmail.com>
2023-03-22 20:52:13 -0400

    Complete palindrome solution 2

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++){
+    const j = word.length - 1 - i
+    
+    if (word[i] !== word[j]) return false
+  }
+  return true
 }
+
+/*
+  make a function that returns true if palindrome. if the first letter is the same
+  as the last letter and the second is the same from the second to last and so on
+*/
 
 /* 
   Add your pseudocode here
+  iterate form the beginning to middle of word
+    check each letter to the corresponding letter to the end 
+      if letters dont match return false
+
+  return true
+
 */
 
 /*
   Add written explanation of your solution here
+  function takes in a word and checkes the first and last letter to check if ===
+  then will move to the second and second to last and so on checking ===
+  if all are === returns true if not returns false
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +41,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  /*
+    abba
+    ^  ^
+     ^^
+  */
 }
 
 module.exports = isPalindrome;
